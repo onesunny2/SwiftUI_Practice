d4442bee28286594fe7429011037a3a686ef2fd1
onesunny2 <ippne996@gmail.com>
2025-05-01 16:46:18 +0900

    first commit: add gitignore, set basic

---
+++ StateSynchronization/StateSynchronization/StateSynchronizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateSynchronizationApp.swift
+//  StateSynchronization
+//
+//  Created by Lee Wonsun on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct StateSynchronizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
