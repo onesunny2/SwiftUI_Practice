72a2e1d5be038815ef96ed7821fdc951977ba2a4
onesunny2 <ippne996@gmail.com>
2025-05-02 15:27:30 +0900

    saveDownloadManager 구조 수정

--- StateSynchronization/StateSynchronization/SaveDownloadManager.swift
+++ StateSynchronization/StateSynchronization/SaveDownloadManager.swift
@@ -21,13 +21,35 @@ enum Coders {
 
 extension Int {
     func forKey() -> String {
-        return String(self)
+        return "AppInfo_\(self)"
     }
 }
 
 final class SaveDownloadManager: ObservableObject {
     
-    @Published var statusById: [Int: DownLoad] = [:]
+    @Published var statusById: [Int: AppInfo] = [:]
+    
+    func loadAllInfo() {
+        
+        let allKeys = UserDefaults.standard.dictionaryRepresentation().keys
+        let appKeys = allKeys.filter { $0.hasPrefix("AppInfo_") }
+        
+        for key in appKeys {
+            
+            guard let idString = key.split(separator: "_").last,
+                  let id = Int(idString),
+                  let data = UserDefaults.standard.data(forKey: key) else { return }
+            
+            do {
+                let info = try Coders.decoder.decode(AppInfo.self, from: data)
+                statusById[id] = info
+            } catch {
+                print("\(id) 정보 불러오기 실패: \(error)")
+            }
+        }
+        
+        print("총 \(statusById.count)개의 앱 정보 로드 완료")
+    }
     
     func saveInfo(id: Int, status: DownLoad, count: CGFloat, date: Date) {
         let data: AppInfo = AppInfo(
@@ -37,25 +59,28 @@ final class SaveDownloadManager: ObservableObject {
             lastTime: date
         )
         
+        statusById[id] = data
+        
         do {
             let saveData = try Coders.encoder.encode(data)
             UserDefaults.standard.set(saveData, forKey: id.forKey())
+            
+            objectWillChange.send()
         } catch {
             print("userdefault 저장 실패: \(error)")
         }
-        
     }
     
-    func loadInfo(for id: Int) -> AppInfo? {
-        
-        guard let data = UserDefaults.standard.data(forKey: id.forKey()) else { return nil }
-        
-        do {
-            let info = try Coders.decoder.decode(AppInfo.self, from: data)
-            return info
-        } catch {
-            print("userdefault 불러오기 실패: \(error)")
-            return nil
-        }
-    }
+//    func loadInfo(for id: Int) -> AppInfo? {
+//        
+//        guard let data = UserDefaults.standard.data(forKey: id.forKey()) else { return nil }
+//        
+//        do {
+//            let info = try Coders.decoder.decode(AppInfo.self, from: data)
+//            return info
+//        } catch {
+//            print("userdefault 불러오기 실패: \(error)")
+//            return nil
+//        }
+//    }
 }
