9f7cec72d27b099c7c78d08f1eedac52ced589d1
onesunny2 <ippne996@gmail.com>
2025-05-04 23:58:54 +0900

    create NetworkObserver File

---
+++ NetworkObserver/NetworkObserver/NetworkObserverApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkObserverApp.swift
+//  NetworkObserver
+//
+//  Created by Lee Wonsun on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkObserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
