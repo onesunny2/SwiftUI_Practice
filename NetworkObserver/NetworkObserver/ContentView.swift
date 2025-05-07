e22b04d068920cd1b60deb7dd65d11220a810391
onesunny2 <ippne996@gmail.com>
2025-05-07 14:40:05 +0900

    현재 네트워크 상태를 알려주는 list 구성

--- NetworkObserver/NetworkObserver/ContentView.swift
+++ NetworkObserver/NetworkObserver/ContentView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.isNetworkConnected) private var isConnected
+    @Environment(\.connetionType) private var connectionType
+    
     var body: some View {
-        Text("Network Observer Practice")
+        NavigationStack {
+            List {
+                Section("Status") {
+                    Text((isConnected ?? false) ? "Connected" : "No Internet")
+                }
+                
+                if let connectionType {
+                    Section("Type") {
+                        Text(String(describing: connectionType).capitalized)
+                    }
+                }
+            }
+            .navigationTitle("Network Monitor")
+        }
+        .sheet(isPresented: .constant(!(isConnected ?? true))) {
+            NoInternetView()
+                .presentationDetents([.height(310)])
+//                .presentationCornerRadius(0)
+//                .presentationBackgroundInteraction(.disabled)
+//                .presentationBackground(.clear)
+                .interactiveDismissDisabled()
+        }
     }
 }
 
