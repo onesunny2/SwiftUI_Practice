e9ce6420354a770c06e3016307c0b318630aab0c
onesunny2 <ippne996@gmail.com>
2025-05-08 02:52:53 +0900

    create LoginInlineToast Project

---
+++ LoginInlineToast/LoginInlineToast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginInlineToast
+//
+//  Created by Lee Wonsun on 5/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginInlineToast/LoginInlineToast/LoginInlineToastApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginInlineToastApp.swift
+//  LoginInlineToast
+//
+//  Created by Lee Wonsun on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginInlineToastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
