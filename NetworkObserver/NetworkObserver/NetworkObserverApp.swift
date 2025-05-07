7a5a435a561e83853b6794f186d7f59bb12474a3
onesunny2 <ippne996@gmail.com>
2025-05-07 14:39:31 +0900

    main app에 networkMonitor와 environment keypath 연결

--- NetworkObserver/NetworkObserver/NetworkObserverApp.swift
+++ NetworkObserver/NetworkObserver/NetworkObserverApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct NetworkObserverApp: App {
+    
+    @StateObject private var networkMonitor = NetworkMonitor()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.isNetworkConnected, networkMonitor.isConnected)
+                .environment(\.connetionType, networkMonitor.connectionType)
         }
     }
 }
